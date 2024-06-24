887e809ea80aad4e65ce069d1f59c3abb24215cf
dev-phyoewai <phyoewai2311@gmail.com>
2024-06-25 00:38:52 +0630

    Starting Created Dashboard In Laravel!

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name' => 'sophia',
+            'email' => 'sophia@gmail.com',
+            'password' => Hash::make('sophia123'),
+        ]);
+
+        $editor = User::create([
+            'name' => 'editor',
+            'email' => 'editor@gmail.com',
+            'password' => Hash::make('editor123'),
+        ]);
+    }
+}
